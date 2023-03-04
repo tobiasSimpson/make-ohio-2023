fcf681f0d650643d8871894e0eee35d87a2bb8fb
acou12 <51276518+acou12@users.noreply.github.com>
2023-03-04 09:08:18 -0500

    Update `coordinates-to-angle.py` to account for tilt and revolution

--- coordinates-to-angle.py
+++ coordinates-to-angle.py
@@ -1,12 +1,11 @@
 from math import *
 
-# (lat, long) = map(float, input().split(", "))
-
 lat = float(input("Lat: ")) * (2 * pi / 360)
 long = float(input("Long: ")) * (2 * pi / 360)
-time = float(input("Time (hours since midnight): "));
+time = float(input("Time (hours since midnight): "))
+revolution = float(input("Rev: ")) * (2 * pi / 360)
 
-long += time * 2 * pi / 24
+long = (time - 12) * 2 * pi / 24 + revolution
 
 EARTH_TILT = 23.4 * 2 * pi / 360
 
@@ -14,7 +13,11 @@ x = cos(long) * cos(lat)
 y = sin(long) * cos(lat)
 z = sin(lat)
 
-print(x, y, z)
+(y, z) = (cos(EARTH_TILT) * y + -sin(EARTH_TILT) * z, sin(EARTH_TILT) * y + cos(EARTH_TILT) * z)
 
-angle = (acos(x) * 360 / (2 * pi))
-print(angle)
+dot = x * cos(revolution) + y * sin(revolution)
+
+if dot <= 0: print("dark side")
+else:
+    angle = (acos(dot) * 360 / (2 * pi))
+    print(90 - angle)
