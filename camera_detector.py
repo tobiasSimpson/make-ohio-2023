7fca63aa377f9f1599a384e25d2a57a9c83d5aa1
tobiasSimpson <93829921+tobiasSimpson@users.noreply.github.com>
2023-03-04 17:08:12 -0500

    accumlation stuff

--- camera_detector.py
+++ camera_detector.py
@@ -3,32 +3,31 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-pTime = 0
-cTime = 0
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,1920)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1080)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,1080)
 
-pTime = 0
-cTime = 0
+success, scores = cap.read()
+scores = cv2.cvtColor(scores, cv2.COLOR_BGR2GRAY)
 
-success, img = cap.read()
 
 while True:
     success, img = cap.read()
-    
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # Create mask to only select white
-    maskR = cv2.inRange(img, np.array([0, 0, 150]), np.array([75, 75, 255]))
-    maskBk = cv2.inRange(img, np.array([200, 200, 200]), np.array([255, 255, 255]))
+    # maskR = cv2.inRange(img, np.array([0, 0, 150]), np.array([75, 75, 255]))
+    # maskBk = cv2.inRange(img, np.array([200, 200, 200]), np.array([255, 255, 255]))
 
-    imgMats = 0
+    # scoresSum = np.sum(scores)
+    # scores = np.append(scores, np.zeros((1280-720, 1280)), axis=0) / scoresSum
+    # img = np.append(img, np.zeros((1280-720, 1280)), axis=0) / 255
+    # scores = np.matmul(scores, img)
 
-    # Change image to grey where we found white
-    # img[maskR > 0] = (255, 255, 255)
-    # img[maskBk > 0] = (150, 150, 150)
+    img2 = np.float32(np.zeros(img.size))
+    img2 = cv2.accumulate(img, img2)
+    img2 = cv2.accumulate(scores, img2)
+    scores = scores/2
 
-    img=cv2.cvtColor(img, cv2.COLORMAP_HOT)
-    print(img[2])
- 
-    cv2.imshow('Reflectance Detection', img)
+    cv2.imshow('Reflectance Detection', scores)
+    print(scores)
     cv2.waitKey(1)
